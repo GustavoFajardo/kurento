e7a9ee17b855f8b9479771ea5df26db2564071da
Gustavo <gustavo.fajardo@advantage.com.co>
2022-06-13 12:15:17 -0500

    'cambios'

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,16 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-if (hasConsole === 'false'){
-$('#console').css('display','none');
-}
+    if (hasConsole === 'false') {
+        $('#console').css('display', 'none');
+    }
 
-if(tipo === 'cliente'){
-$('#buttonCall').remove();
-$('#register').text('Acceda a la fila');
-}
+    if (tipo === 'cliente') {
+        $('#buttonCall').remove();
+        $('#register').text('Acceda a la fila');
+        if (screen.width>414) {
+            $('#funcPanel').css('left', '360px');
+        }
+
+    }
 
 });
